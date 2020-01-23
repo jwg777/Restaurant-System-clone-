61592399ba8d2af9495a9e606e56bcfd947e57e3
ZFAC156 <ZFAC156@live.rhul.ac.uk>
2020-01-23 23:07:20 +0000

    Didn't fully commit from last one

---
+++ src/views/ButtonController.java
@@ -0,0 +1,45 @@
+package views;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public final class ButtonController {
+	
+	private static ButtonController instance = null;
+	Stage stage;
+	
+	public static ButtonController getInstance() {
+		if(instance == null) {
+			instance = new ButtonController();
+		}
+		return instance;
+	}
+	
+	private ButtonController() {
+	}
+	
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+	
+
+	public void startCustomer() throws Exception{
+		Parent customerViewParent = FXMLLoader.load(getClass().getResource("CustomerView.fxml"));
+		Scene customerScene = new Scene(customerViewParent);
+		stage.setTitle("Oaxaca Customer View");
+		stage.setScene(customerScene);
+		stage.show();
+	}
+	
+	public void startMain() throws Exception {
+	    Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+	    Scene mainScene = new Scene(root, 362, 213);
+	    stage.setTitle("Oaxaca Management System");
+	    stage.setScene(mainScene);
+	    stage.show();
+	}
+	
+
+}
