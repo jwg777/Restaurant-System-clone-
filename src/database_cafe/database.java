4270b3b4e22392699124ac8fb12e5baf4cc8bcdd
joshuagargan <zfac209@live.ac.uk>
2020-01-23 14:10:10 +0000

    Adjusted tables, exapnded on current tables created and created more. Added more columns etc to more closely match how the data will be represented in the project.

--- src/database_cafe/database.java
+++ src/database_cafe/database.java
@@ -6,31 +6,31 @@ import java.util.Scanner;
 public class database {
 
   public static void main(String[] args) throws SQLException {
-    
-    String user = "";
-    String password = ""; 
-    
+     
+    String user = "", password = "";
     System.out.println("Please enter your username");
     Scanner scan = new Scanner(System.in);
     user = scan.nextLine();
     System.out.println("Please enter your password");
     password = scan.nextLine();
     scan.close();
-     
+ 
     String database = "//teachdb.cs.rhul.ac.uk/CS2855/";
-    
-    Connection connection = connectToDatabase(user, password, database); 
-    
+   
+    Connection connection = connectToDatabase(user, password, database);
+   
     if(connection != null) {
       System.out.println("Database is activated!");
     } else {
       System.out.println("Error occurred, access denied!");
       return;
     }
-    createTable(connection, "Orders(orderID int primary key, price float, orderTimes time)");
-    createTable(connection, "Customers(customerID varchar(20) primary key, password varchar(20), tableNumber int, orderID int references Orders(orderID))");
-    createTable(connection, "Waiters(waitersID varchar(20) primary key, password varchar(20), tableNumber int)");
-    createTable(connection, "Kitchen(kitchenID varchar(20) primary key, password varchar(20), orderID int references Orders(orderID))");
+    createTable(connection, "Customers(cust_id int primary key, password varchar(20), tableNumber int)");
+    createTable(connection, "Menu(dish varChar(100) primary key, price float, info varchar(300))");
+    createTable(connection, "Orders(orderID int primary key, cust_id int, total_price float, orderTime Timestamp, dish varchar(100), status varchar(100), foreign key (cust_id) references Customers(cust_id) on delete cascade,"
+            + " foreign key (dish) references Menu(dish) on delete cascade)");
+    createTable(connection, "Staff(staff_id int primary key, password varchar(100), role varchar(20))");
+    createTable(connection, "Issues(issue_id int primary key, issue_note varchar(200), cust_id int, foreign key (cust_id) references Customers(cust_id) on delete cascade)");
   }
 
   public static Connection connectToDatabase(String user, String password, String database) {
