6c63481488c29e4c4c472bb0133da0a6de0257bd
imran <imran@10.2.153.93>
2020-01-23 14:52:15 +0000

    Customer and CustomerView.fxml

--- src/views/Customer.java
+++ src/views/Customer.java
@@ -1,5 +1,25 @@
 package views;
 
-public class CustomerView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Customer extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+      Parent root = FXMLLoader.load(getClass().getResource("CustomerView.fxml"));
+    
+      Scene scene = new Scene(root, 362, 213);
+    
+      stage.setTitle("Oaxaca Customer View");
+      stage.setScene(scene);
+      stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
