156cf161d7d8981d49b3c70a3875006a6f856b4f
joshuagargan <zfac209@live.ac.uk>
2020-01-23 12:29:36 +0000

    Compacted the code. Removed drop table method and streamlined it within create table to hide the need for dropping the table.

--- src/database_cafe/database.java
+++ src/database_cafe/database.java
@@ -6,63 +6,62 @@ import java.util.Scanner;
 public class database {
 
   public static void main(String[] args) throws SQLException {
+    
     String user = "";
-    String password = "";
+    String password = ""; 
+    
     System.out.println("Please enter your username");
     Scanner scan = new Scanner(System.in);
     user = scan.nextLine();
     System.out.println("Please enter your password");
     password = scan.nextLine();
     scan.close();
-    String database = "teachdb.cs.rhul.ac.uk";
-    Connection connection = connectToDatabase(user, password, database);  
+     
+    String database = "//teachdb.cs.rhul.ac.uk/CS2855/";
+    
+    Connection connection = connectToDatabase(user, password, database); 
+    
     if(connection != null) {
       System.out.println("Database is activated!");
     } else {
       System.out.println("Error occurred, access denied!");
       return;
     }
-    dropTable(connection, "Customers");
-    dropTable(connection, "Waiters");
-    dropTable(connection, "Kitchen");
-    dropTable(connection, "Orders");
     createTable(connection, "Orders(orderID int primary key, price float, orderTimes time)");
     createTable(connection, "Customers(customerID varchar(20) primary key, password varchar(20), tableNumber int, orderID int references Orders(orderID))");
     createTable(connection, "Waiters(waitersID varchar(20) primary key, password varchar(20), tableNumber int)");
     createTable(connection, "Kitchen(kitchenID varchar(20) primary key, password varchar(20), orderID int references Orders(orderID))");
   }
-	
+
   public static Connection connectToDatabase(String user, String password, String database) {
     System.out.println("~~~~~~~~~~~~~~~ PostgreSQL___JDBC Connection Testing ~~~~~~~~~~~~~~~");
     Connection connection = null;
     try {
-      connection = DriverManager.getConnection("jdbc:postgresql://teachdb.cs.rhul.ac.uk/CS2855/" + user, user, password);
+      connection = DriverManager.getConnection("jdbc:postgresql:" + database + user, user, password);
     } catch (SQLException e) {
       System.out.println("Connection failed! Check output console");
       e.printStackTrace();
     }
     return connection;
   }
-  
+ 
   public static void createTable(Connection connection, String tableName) {
     Statement st = null;
+    String table = "";
+    for (int i = 0; i < tableName.length(); i++) {
+        if (tableName.charAt(i) == '(') {
+            table = tableName.substring(0, i);
+            break;
+        }
+    }
     try {
       st = connection.createStatement();
+      st.execute("DROP TABLE IF EXISTS " + table + " CASCADE");
       st.execute("CREATE TABLE " + tableName);
       st.close();
     } catch (SQLException e) {
       e.printStackTrace();
     }
   }
-  
-  public static void dropTable(Connection connection, String tableName) {
-    Statement st = null;
-    try {
-      st = connection.createStatement();
-      st.execute("DROP TABLE IF EXISTS " + tableName);
-      st.close();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
+
 }
