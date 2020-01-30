6d0981bf7d822d9c3bac49b0baeb5fb85a474780
sean112533 <zdte141@live.rhul.ac.uk>
2020-01-30 13:34:49 +0000

    error occur is fixed for opening the interface in waiter_view

--- src/views/WaiterViewController.java
+++ src/views/WaiterViewController.java
@@ -2,6 +2,14 @@ package views;
 
 import javafx.fxml.FXML;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 /**
  * Controller for the waiter view..
  */
@@ -9,8 +17,7 @@ public class WaiterViewController {
   
   /** The button controller. */
   ButtonController butController = ButtonController.getInstance();
-  
-  
+ 
   /**
    * When the 'Return to Main Menu button is pressed, return to the main menu.
    *
@@ -22,6 +29,5 @@ public class WaiterViewController {
   }
   
   
-  
-  
+ 
 }
--- test/testConsumables/testConsumable.java
+++ test/testConsumables/testConsumable.java
@@ -2,6 +2,7 @@ package testConsumables;
 
 import static org.junit.Assert.assertEquals;
 
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
