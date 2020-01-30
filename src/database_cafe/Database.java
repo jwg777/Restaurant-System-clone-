a726d77f1f5f8c02f5e6a7c5c7e907150022bfbb
theas <Ashhabur.Chowdhury.2018@live.rhul.ac.uk>
2020-01-30 09:11:58 +0000

    Changed to make it possible to use with the interface.

--- src/database_cafe/Database.java
+++ src/database_cafe/Database.java
@@ -18,13 +18,13 @@ import java.util.Scanner;
 
 public class Database {
 
-  /** takes information to connect to the database and call methods to create and fill tables.
+  static Connection connection = null;
+
+  /** Constructor for the database object, creating and filling in initial values.
    * 
-   * @param args standard parameter for the main
-   * @throws SQLException thrown if an error occurs when connecting to the database
-   * 
+   * @throws SQLException Thrown if  a problem occurs with communicating with the database server.
    */
-  public static void main(String[] args) throws SQLException {
+  public Database() throws SQLException {
     
     String user = "";
     System.out.println("Please enter your username");
@@ -38,7 +38,7 @@ public class Database {
     String database = "//localhost/CS2855/"; 
     //change localhost to teachdb.cs.rhul.ac.uk if using nomachine.
    
-    Connection connection = connectToDatabase(user, password, database);
+    connection = connectToDatabase(user, password, database);
    
     if (connection != null) {
       System.out.println("Database is activated!");
@@ -46,13 +46,13 @@ public class Database {
       System.out.println("Error occurred, access denied!");
       return;
     }
-    createTable(connection, "Customers(cust_id int primary key, "
+    createTable("Customers(cust_id int primary key, "
         + "password varchar(20), "
         + "tableNumber int)");
-    createTable(connection, "Menu(dish varChar(100) primary key, "
+    createTable("Menu(dish varChar(100) primary key, "
         + "price float, "
         + "info varchar(300))");
-    createTable(connection, "Orders(orderID int primary key, "
+    createTable("Orders(orderID int primary key, "
         + "cust_id int, "
         + "total_price float, "
         + "orderTime Timestamp, "
@@ -60,10 +60,10 @@ public class Database {
         + "status varchar(100), "
         + "foreign key (cust_id) references Customers(cust_id) on delete cascade,"
         + " foreign key (dish) references Menu(dish) on delete cascade)");
-    createTable(connection, "Staff(staff_id int primary key, "
+    createTable("Staff(staff_id int primary key, "
         + "password varchar(100), "
         + "role varchar(20))");
-    createTable(connection, "Issues(issue_id int primary key, "
+    createTable("Issues(issue_id int primary key, "
         + "issue_note varchar(200), "
         + "cust_id int, foreign key (cust_id) references Customers(cust_id) on delete cascade)");
     File menuFile = new File("Menu");
@@ -83,13 +83,12 @@ public class Database {
           }
         }
         line += "'";
-        insertIntoTable(connection, "Menu", "", line);
+        insertIntoTable("Menu", "", line);
         line = br.readLine();
       }
     } catch (Exception e) {
       e.printStackTrace();
     }
-    viewMenu(connection);
   }
 
   /** This is the method used to create a connection to the database.
@@ -102,7 +101,6 @@ public class Database {
    */
   public static Connection connectToDatabase(String user, String password, String database) {
     System.out.println("~~~~~~~~~~~~~~~ PostgreSQL___JDBC Connection Testing ~~~~~~~~~~~~~~~");
-    Connection connection = null;
     try {
       connection = 
         DriverManager.getConnection("jdbc:postgresql:" + database + user, user, password);
@@ -115,11 +113,10 @@ public class Database {
  
   /** This is the method used to create a table in the database.
    * 
-   * @param connection this is the connection to the database server
    * @param tableName given for the new table to be created
    * 
    */
-  public static void createTable(Connection connection, String tableName) {
+  public static void createTable(String tableName) {
     Statement st = null;
     String table = "";
     for (int i = 0; i < tableName.length(); i++) {
@@ -140,14 +137,12 @@ public class Database {
   
   /** This method is used to insert data into an existing table.
    * 
-   * @param connection this is the connection to the database
    * @param tableName for the table that is having data put inside it
    * @param attributes optional, if only specific fields want to be filled in
    * @param values this is the data to be input in string format
    * 
    */
-  public static void insertIntoTable(Connection connection,
-      String tableName, String attributes, String values) {
+  public static void insertIntoTable(String tableName, String attributes, String values) {
     Statement st = null;
     try {
       st = connection.createStatement();
@@ -164,12 +159,11 @@ public class Database {
   
   /** This is used when executing a query to store the result.
    * 
-   * @param connection is the connection to the database
    * @param query is the query to be executed
    * @return will return the result set of executing the query
    * 
    */
-  public static ResultSet select(Connection connection, String query) {
+  public static ResultSet executeQuery(String query) {
     Statement st = null;
     ResultSet rs = null;
     try {
@@ -181,28 +175,12 @@ public class Database {
     return rs;
   }
   
-  /** This method is used to select from the menu table, to show the contents of the menu.
+  /** Method to get connection to the database.
    * 
-   * @param connection is the connection to the database
+   * @return will return the connection to the database
    */
-  public static void viewMenu(Connection connection) {
-    System.out.println("~~~~~~~~~~~~~~~~~Menu~~~~~~~~~~~~~~~~");
-    String query = "SELECT * " 
-                 + "FROM Menu ";
-    ResultSet rs = select(connection, query);
-    try {
-      while (rs.next()) {
-        for (int i = 1; i < 4; i++) {
-          if (i == 2) {
-            System.out.print("£");
-          }
-          System.out.print(rs.getString(i) + " ");
-        }
-        System.out.println("");
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
+  public Connection getConnection() {
+    return connection;
   }
 
 }
