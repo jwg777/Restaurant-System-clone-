39a094637ae48ee4e502e716c7d7c51dce1be3bf
zfac005 <zfac005@cim-ts-node-02.cc.rhul.local>
2020-01-31 08:05:15 +0000

    Same as previous commit, instead actually changing db code this time!

--- src/database_cafe/Database.java
+++ src/database_cafe/Database.java
@@ -9,7 +9,9 @@ import java.io.FileReader;
 
 public class Database {
 	
-	public String[] connInfo() {
+	
+	
+	public String[] connInfoSetup() {
 		String user = "", password = "";
 	    System.out.println("Please enter your username");
 	    Scanner scan = new Scanner(System.in);
@@ -20,54 +22,72 @@ public class Database {
 	 
 	    //String database = "//localhost/CS2855/";
 	    String database = "//teachdb.cs.rhul.ac.uk/CS2855/";
+	    
+	    String[] connInfo = new String[3];
+	    
+	    connInfo[0] = user;
+	    connInfo[1] = password;
+	    connInfo[2] = database;
+	    
+	    return connInfo;
+	    
+	}
+	
+	public void buildTables(Connection connection) {
+		createTable(connection, "Customers(cust_id int primary key, password varchar(20), tableNumber int)");
+	    createTable(connection, "Menu(dish varChar(100) primary key, price float, info varchar(300), type varchar(100))");
+	    createTable(connection, "Orders(orderID int primary key, cust_id int, total_price float, orderTime Timestamp, dish varchar(100), status varchar(100), foreign key (cust_id) references Customers(cust_id) on delete cascade,"
+	            + " foreign key (dish) references Menu(dish) on delete cascade)");
+	    createTable(connection, "Staff(staff_id int primary key, password varchar(100), role varchar(20))");
+	    createTable(connection, "Issues(issue_id int primary key, issue_note varchar(200), cust_id int, foreign key (cust_id) references Customers(cust_id) on delete cascade)");
+	    
+	}
+	
+	public void importFile(Connection connection, String file0) {
+		File inputFile = new File(file0);
+	    String line = "";
+	    BufferedReader br;
+	    try {
+	    	  br = new BufferedReader(new FileReader(inputFile));
+	    	  line = br.readLine();
+	    	  while(line != null) {
+	    		String[] splitRead = line.split(">");
+	    		line = "'";
+	    		for(int i = 0; i < splitRead.length; i++) {
+	    	      if(i == 0) {
+	    		    line += splitRead[i];
+	    	      } else {
+	    	        line += "', '" + splitRead[i];
+	    	      }
+	    		}
+	    		line += "'";
+	    		insertIntoTable(connection, "Menu", "", line);
+	    	    line = br.readLine();
+	    	  }
+	    	} catch (Exception e) {
+	    	  e.printStackTrace();
+	    	}
 	}
 
-  public static void main(String[] args) throws SQLException {
-    //test 
-    
-   
-    Connection connection = connectToDatabase(user, password, Database);
-   
-    if(connection != null) {
-      System.out.println("Database is activated!");
-    } else {
-      System.out.println("Error occurred, access denied!");
-      return;
-    }
-    createTable(connection, "Customers(cust_id int primary key, password varchar(20), tableNumber int)");
-    createTable(connection, "Menu(dish varChar(100) primary key, price float, info varchar(300))");
-    createTable(connection, "Orders(orderID int primary key, cust_id int, total_price float, orderTime Timestamp, dish varchar(100), status varchar(100), foreign key (cust_id) references Customers(cust_id) on delete cascade,"
-            + " foreign key (dish) references Menu(dish) on delete cascade)");
-    createTable(connection, "Staff(staff_id int primary key, password varchar(100), role varchar(20))");
-    createTable(connection, "Issues(issue_id int primary key, issue_note varchar(200), cust_id int, foreign key (cust_id) references Customers(cust_id) on delete cascade)");
-    
-    File menuFile = new File("Menu");
-    String line = "";
-    BufferedReader br;
-    try {
-    	  br = new BufferedReader(new FileReader(menuFile));
-    	  line = br.readLine();
-    	  while(line != null) {
-    		String[] splitRead = line.split(">");
-    		line = "'";
-    		for(int i = 0; i < splitRead.length; i++) {
-    	      if(i == 0) {
-    		    line += splitRead[i];
-    	      } else {
-    	        line += "', '" + splitRead[i];
-    	      }
-    		}
-    		line += "'";
-    		insertIntoTable(connection, "Menu", "", line);
-    	    line = br.readLine();
-    	  }
-    	} catch (Exception e) {
-    	  e.printStackTrace();
-    	}
-    //ViewMenu(connection);
-  }
+//  public static void main(String[] args) throws SQLException {
+//    //test 
+//    
+//   //connection test
+//    Connection connection = connectToDatabase(user, password, database);
+//    if(connection != null) {
+//      System.out.println("Database is activated!");
+//    } else {
+//      System.out.println("Failed to make connection!");
+//      return;
+//    }
+//    
+//    buildTables();
+//    
+//    
+//    //ViewMenu(connection);
+//  }
 
-  public static Connection connectToDatabase(String user, String password, String database) {
+  public Connection connectToDatabase(String user, String password, String database) {
     System.out.println("~~~~~~~~~~~~~~~ PostgreSQL___JDBC Connection Testing ~~~~~~~~~~~~~~~");
     Connection connection = null;
     try {
@@ -79,7 +99,7 @@ public class Database {
     return connection;
   }
  
-  public static void createTable(Connection connection, String tableName) {
+  public void createTable(Connection connection, String tableName) {
     Statement st = null;
     String table = "";
     for (int i = 0; i < tableName.length(); i++) {
@@ -98,7 +118,7 @@ public class Database {
     }
   }
   
-  public static void insertIntoTable(Connection connection, String tableName, String attributes, String values) {
+  public void insertIntoTable(Connection connection, String tableName, String attributes, String values) {
     Statement st = null;
     try {
       st = connection.createStatement();
@@ -113,7 +133,7 @@ public class Database {
     }
   }
   
-  public static ResultSet Select(Connection connection, String query) {
+  public ResultSet Select(Connection connection, String query) {
     Statement st = null;
     ResultSet rs = null;
     try {
@@ -125,24 +145,24 @@ public class Database {
     return rs;
   }
   
-  public static void ViewMenu(Connection connection) {
-    System.out.println("~~~~~~~~~~~~~~~~~Menu~~~~~~~~~~~~~~~~");
-    String query = "SELECT * " +
-                   "FROM Menu ";
-    ResultSet rs = Select(connection, query);
-    try {
-		while(rs.next()) {
-		  for(int i = 1; i < 4; i++) {
-			if(i == 2) {
-			  System.out.print("£");
-			}
-		    System.out.print(rs.getString(i) + " ");
-		  }
-		  System.out.println("");
-		}
-	} catch (SQLException e) {
-		e.printStackTrace();
-	}
-  }
+//  public static void ViewMenu(Connection connection) {
+//    System.out.println("~~~~~~~~~~~~~~~~~Menu~~~~~~~~~~~~~~~~");
+//    String query = "SELECT * " +
+//                   "FROM Menu ";
+//    ResultSet rs = Select(connection, query);
+//    try {
+//		while(rs.next()) {
+//		  for(int i = 1; i < 4; i++) {
+//			if(i == 2) {
+//			  System.out.print("£");
+//			}
+//		    System.out.print(rs.getString(i) + " ");
+//		  }
+//		  System.out.println("");
+//		}
+//	} catch (SQLException e) {
+//		e.printStackTrace();
+//	}
+//  }
 
 }
