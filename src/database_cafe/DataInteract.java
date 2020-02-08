00c0519ebacfc1ee1ea30da6e35c69cbd3915b78
imran <imran@DESKTOP-RGR49O3>
2020-02-08 22:18:24 +0000

    Error Fixes
    Database errors in customer view have now been fixed from adjustment in
    DataInteract class and menu file

--- src/database_cafe/DataInteract.java
+++ src/database_cafe/DataInteract.java
@@ -13,7 +13,7 @@ public final class DataInteract {
     
     database = new Database();
     database.createTable("Customers(cust_id int primary key, password varchar(20), tableNumber int)");
-    database.createTable("Menu(dish varChar(100) primary key, price numeric(4, 2), info varchar(300), allergens varchar(500), calories int, type varchar(100))");
+    database.createTable("Menu(dish varChar(100) primary key, price numeric(4, 2), allergens varchar(500), calories int, type varchar(100))");
     database.createTable("Orders(orderID int primary key, cust_id int, total_price float, orderTime Timestamp, dish varchar(100), status varchar(100), foreign key (cust_id) references Customers(cust_id) on delete cascade,"
             + " foreign key (dish) references Menu(dish) on delete cascade)");
     database.createTable("Staff(staff_id int primary key, password varchar(100), role varchar(20))");
