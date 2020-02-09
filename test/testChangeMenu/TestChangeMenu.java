f9327a5144546e71f28a76e389fdc91393d21bd0
theas <Ashhabur.Chowdhury.2018@live.rhul.ac.uk>
2020-02-09 15:47:34 +0000

    Created a test class and test methods for AddDeleteViewController.

---
+++ test/testChangeMenu/TestChangeMenu.java
@@ -0,0 +1,40 @@
+package testChangeMenu;
+/** Testing for the AddDeleteViewController
+ * 
+ * @author : TeamProject 2020 group 22
+ * 
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class TestChangeMenu {
+  
+  /** Set up the database and controllers beforehand.
+   * 
+   * @throws Exception thrown if controllers or database can not be instantiated.
+   * 
+   */
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {
+    //TO-DO
+  }
+
+  /** Test that items can be added to the menu.
+   * 
+   */
+  @Test
+  void testAddItem() {
+    fail("Not yet implemented");
+  }
+  
+  /** Test that items can be deleted from the menu.
+   * 
+   */
+  @Test
+  void testDeleteItem() {
+    fail("Not yet implemented");
+  }
+
+}
