1a424ae174eda359bda008cc7cbd282e14affb73
Dipo Rodipe <zfac005@live.rhul.ac.uk>
2020-02-10 16:04:57 +0000

    changed allergen info in the Menu file to be comma-seperated and created a test class for the Customer Access class

---
+++ testBackend/testCustomerAccess.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testCustomerAccess {
+
+  @BeforeEach
+  void setUp() throws Exception {}
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
