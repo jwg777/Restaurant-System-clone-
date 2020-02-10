3729c0602d26b31b46160c1997231a6046e7994a
sean112533 <zdte141@live.rhul.ac.uk>
2020-02-10 13:12:22 +0000

    fix the error

--- src/backend/LoginAccess.java
+++ src/backend/LoginAccess.java
@@ -5,13 +5,13 @@ import java.sql.SQLException;
 import database_cafe.DataInteract;
 
 public class LoginAccess {
-  
+
   DataInteract loginData;
-  
+
   public LoginAccess() {
     loginData = DataInteract.getInstance();
   }
-  
+
   public boolean checkUser(String username, String password) throws SQLException {
     ResultSet rs = loginData.select("SELECT * FROM Staff");
     while (rs.next()) {
@@ -19,10 +19,10 @@ public class LoginAccess {
         return true;
       }
     }
-    
+
     return false;
   }
-  
+
   public boolean isKitchen(String username) throws SQLException {
     ResultSet rs = loginData.select("SELECT * FROM Staff");
     while (rs.next()) {
--- src/views/WaiterViewController.java
+++ src/views/WaiterViewController.java
@@ -21,6 +21,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 
 /**
