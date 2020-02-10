021a48c199365aa1d83296adab0c53e0e06b7a8f
Dipo Rodipe <zfac005@live.rhul.ac.uk>
2020-02-10 16:35:17 +0000

    created test for retrieving allergens from the database

--- testBackend/testCustomerAccess.java
+++ testBackend/testCustomerAccess.java
@@ -1,15 +1,28 @@
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import backend.CustomerAccess;
 
 class testCustomerAccess {
+  // Note: the following are templates for using assert functions in JUnit 5, as seen from my
+  // software engineering coursework
+  // assertEquals(actualResult, expectedResult, "error message")
+
+
 
   @BeforeEach
-  void setUp() throws Exception {}
+  CustomerAccess customerData = new CustomerAccess();
+  String[] exampleList = {"Allergen1", "Allergen2", "Allergen3"};
+
+  void setUp() throws Exception {
+
+  }
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  void testGetAllergens() {
+    String[] myAllergenList = customerData.getAllergens("Apple");
+    assertEquals(myAllergenList, exampleList,
+        "the allergens from the database don't match the expected values");
   }
 
 }
