372374a1ea84d40865a10ae0a8b8a6d9994aed16
Dipo Rodipe <zfac005@live.rhul.ac.uk>
2020-02-12 21:06:48 +0000

    Added template javadoc for LoginAccess class

--- src/backend/LoginAccess.java
+++ src/backend/LoginAccess.java
@@ -4,14 +4,30 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import database_cafe.DataInteract;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class LoginAccess.
+ */
 public class LoginAccess {
 
+  /** The login data. */
   DataInteract loginData;
 
+  /**
+   * Instantiates a new login access.
+   */
   public LoginAccess() {
     loginData = DataInteract.getInstance();
   }
 
+  /**
+   * Check user.
+   *
+   * @param username the username
+   * @param password the password
+   * @return true, if successful
+   * @throws SQLException the SQL exception
+   */
   public boolean checkUser(String username, String password) throws SQLException {
     ResultSet rs = loginData.select("SELECT * FROM Staff");
     while (rs.next()) {
@@ -23,6 +39,13 @@ public class LoginAccess {
     return false;
   }
 
+  /**
+   * Checks if is kitchen.
+   *
+   * @param username the username
+   * @return true, if is kitchen
+   * @throws SQLException the SQL exception
+   */
   public boolean isKitchen(String username) throws SQLException {
     ResultSet rs = loginData.select("SELECT * FROM Staff");
     while (rs.next()) {
