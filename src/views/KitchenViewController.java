e739214677ef9c3668cc8726558b92849f90f666
sean112533 <zdte141@live.rhul.ac.uk>
2020-02-19 17:46:32 +0000

    setUp kitchenView java.class set the sizes

--- src/views/KitchenViewController.java
+++ src/views/KitchenViewController.java
@@ -5,12 +5,14 @@ import consumable.Consumable;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 /**
@@ -20,11 +22,11 @@ public class KitchenViewController {
 
   /** The menu tab pane. */
   @FXML
-  private TabPane menuTabPane;
+  private TabPane orderTabPane = new TabPane();
 
   /** The vbox new. */
   @FXML
-  private VBox vboxNew;
+  private VBox vboxNew = new VBox();
 
   /** The vbox in progress. */
   @FXML
@@ -55,7 +57,7 @@ public class KitchenViewController {
     butController.startMain();
   }
 
-  private VBox createVBox(ArrayList<Consumable> consumables) {
+  private VBox createNewOrderVBox(ArrayList<Consumable> consumables) {
     VBox vbox = new VBox();
     for (Consumable consumable : consumables) {
       HBox tempHBox = new HBox(); // Layout for one consumable of the list
@@ -71,26 +73,26 @@ public class KitchenViewController {
     return vbox;
   }
 
-  private Node initialiseLabel(String name, int i, int j) {
-    // TODO Auto-generated method stub
-    return null;
+  private Node initialiseLabel(String name, int width, int height) {
+    Label label = new Label(name);
+    label.setPrefSize(width, height);
+    return label;
   }
 
   private Node initialiseGap() {
-    // TODO Auto-generated method stub
-    return null;
+    Pane gap = new Pane();
+    gap.setPrefSize(25, 50);
+    return gap;
   }
 
-  @FXML
-  private Tab createTab(String name, ArrayList<Consumable> list) {
+  private Tab createNewOrderTab(String name, ArrayList<Consumable> list) {
     AnchorPane anchorpane = new AnchorPane();
-    anchorpane.setMaxWidth(580);
-    anchorpane.getChildren().add(createVBox(list));
+    anchorpane.setPrefWidth(580);
+    anchorpane.getChildren().add(createNewOrderVBox(list));
     ScrollPane scrollpane = new ScrollPane(anchorpane);
-    scrollpane.setMaxWidth(600);
-    Tab tab = new Tab(name.toUpperCase());
+    scrollpane.setPrefWidth(600);
+    Tab tab = new Tab(name.toUpperCase(), scrollpane);
     return tab;
-
   }
 
 
