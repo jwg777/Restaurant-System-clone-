11c6681d32c2036515ef32652d68e7e992011dae
sean112533 <59835144+sean112533@users.noreply.github.com>
2020-02-27 21:36:30 +0000

    Revert "Kitchen order confirm"

--- src/views/KitchenViewController.java
+++ src/views/KitchenViewController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -30,6 +31,21 @@ public class KitchenViewController {
   @FXML
   private TabPane orderTabPane = new TabPane();
 
+  /** The vbox new. */
+  @FXML
+  private VBox vboxNew = new VBox();
+
+  /** The vbox in progress. */
+  @FXML
+  private VBox vboxInProgress = new VBox();
+
+  /** The vbox completed. */
+  @FXML
+  private VBox vboxCompleted = new VBox();
+
+  /** The ordered list. */
+  @FXML
+  private ListView<?> orderedList;
 
   /** The reload. */
   @FXML
@@ -55,6 +71,7 @@ public class KitchenViewController {
    */
   @FXML
   private void initialize() throws Exception {
+
     newOrderReload();
   }
 
@@ -63,13 +80,13 @@ public class KitchenViewController {
    * 
    * @throws Exception
    */
-  @FXML
   private void newOrderReload() throws Exception {
     order.clear();
     waiterData.getMenu();
     OrderTabPane.getTabs().clear();
     createOrders(order);
   }
+
 
   /**
    * When the 'Return to Main Menu button is pressed, return to the main menu.
