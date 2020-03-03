117e7184561d86762e5dd96762b8c2616dfc4e9c
Dipo Rodipe <zfac005@live.rhul.ac.uk>
2020-03-03 22:48:44 +0000

    implemented code for example 2-1

--- dipoTraining/javaFX-stuff/login-form/src/application/Main.java
+++ dipoTraining/javaFX-stuff/login-form/src/application/Main.java
@@ -13,6 +13,9 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setTitle("JavaFX Welcome");
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
