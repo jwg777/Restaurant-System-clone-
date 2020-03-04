f815218ff0fca8b68e4bf25c01b4882d20aa2951
Dipo Rodipe <zfac005@live.rhul.ac.uk>
2020-03-04 00:01:22 +0000

    implemented code for example 2-6

--- dipoTraining/javaFX-stuff/login-form/src/application/Main.java
+++ dipoTraining/javaFX-stuff/login-form/src/application/Main.java
@@ -1,6 +1,8 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
@@ -12,6 +14,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -39,6 +42,15 @@ public class Main extends Application {
 			hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
 			hbBtn.getChildren().add(btn);
 			grid.add(hbBtn, 1, 4);
+			
+			btn.setOnAction(new EventHandler<ActionEvent>() {
+				 
+			    @Override
+			    public void handle(ActionEvent e) {
+			        actiontarget.setFill(Color.FIREBRICK);
+			        actiontarget.setText("Sign in button pressed");
+			    }
+			});
 			
 			Text scenetitle = new Text("Welcome");
 			scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
