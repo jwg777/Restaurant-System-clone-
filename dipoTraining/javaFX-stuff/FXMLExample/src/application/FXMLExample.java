0e4181888cfa70c2220fd7a4c782f9128efc81c5
Dipo Rodipe <zfac005@live.rhul.ac.uk>
2020-03-12 21:00:53 +0000

    implemented code from example 4-1 of JavaFX tutorial 4

--- dipoTraining/javaFX-stuff/FXMLExample/src/application/FXMLExample.java
+++ dipoTraining/javaFX-stuff/FXMLExample/src/application/FXMLExample.java
@@ -10,11 +10,13 @@ public class FXMLExample extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+		    
+	        Scene scene = new Scene(root, 300, 275);
+	    
+	        stage.setTitle("FXML Welcome");
+	        stage.setScene(scene);
+	        stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
