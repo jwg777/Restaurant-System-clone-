cf4e06ecb540e5a1dd1e606d5bd9b24608e92608
Dipo Rodipe <zfac005@live.rhul.ac.uk>
2020-03-12 21:07:08 +0000

    small fix to imports to remove eclipse warnings

--- dipoTraining/javaFX-stuff/FXMLExample/src/application/FXMLExample.java
+++ dipoTraining/javaFX-stuff/FXMLExample/src/application/FXMLExample.java
@@ -1,14 +1,15 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class FXMLExample extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
 		    
