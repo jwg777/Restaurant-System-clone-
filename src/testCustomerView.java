ebcea961ef1f604ebdc6a7ca787e12cdbc7c0c11
joshuagargan <zfac209@live.rhul.ac.uk>
2020-03-19 14:20:14 +0000

    Added test to testCustomerView which tests customerAccess exists.
    First test just tests if the customerAccess class exists. Also set up
    the test class with a Before tag to prepare testing the database.

--- src/testCustomerView.java
+++ src/testCustomerView.java
@@ -1,11 +1,19 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import backend.CustomerAccess;
 
 class testCustomerView {
 
+  CustomerAccess customerData;
+ 
+  //test customerData method successfully connects to database.
+  @Before
+  public void setUp() {
+    customerData = new CustomerAccess();
+  }
   @Test
-  void test() {
-    fail("Not yet implemented");
+  void test1() {
   }
 
 }
