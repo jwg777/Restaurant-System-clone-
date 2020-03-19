e566eebc5d49da6594c38ab9cfa6bd646d60f0b2
joshuagargan <zfac209@live.rhul.ac.uk>
2020-03-19 13:04:10 +0000

    Creraterd 3 new Junit test classes.
    Each test class refers to each view (customer, kitchen, waiter) and will
    be used to check that it performs correctly on a functional and data
    focused level.

---
+++ test/testKitchenView.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class testKitchenView {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
