80d0501f8aeab2ddc83dcf5bdcacc8d244e84da6
Imran Chagani <zfac032@live.rhul.ac.uk>
2020-04-15 01:14:24 +0100

    oh lawd

--- src/views/CustomerViewController.java
+++ src/views/CustomerViewController.java
@@ -694,5 +694,10 @@ public class CustomerViewController {
   }
   
   @FXML
-  private void submitReview(ActionEvent event) throws IOException {
+  private void cashB(ActionEvent event) throws IOException {
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Cash Payment");
+    alert.setHeaderText(
+        "Someone is coming to take your cash");
+  }
 }
